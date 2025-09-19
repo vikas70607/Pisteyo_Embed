beaaebf76fd8f211aa954942ff3734e9a97171a8
VIKAS <97907750+vikas70607@users.noreply.github.com>
2025-09-20 03:36:57 +0530

    Create hasa.js

---
+++ hasa.js
@@ -0,0 +1,30 @@
+window.botpress.init({
+    "botId": "560191e9-30df-437a-8911-2f60d2da17a2",
+    "configuration": {
+        "version": "v1",
+        "botName": "Crystal",
+        "botAvatar": "https://files.bpcontent.cloud/2025/08/20/14/20250820140512-MF0UMJSB.png",
+        "botDescription": "By using the HASA / Orendatech agent you agree to our privacy terms.",
+        "fabImage": "https://files.bpcontent.cloud/2025/08/20/14/20250820140512-MF0UMJSB.png",
+        "website": {},
+        "email": {},
+        "phone": {},
+        "termsOfService": {},
+        "privacyPolicy": {},
+        "color": "#3276EA",
+        "variant": "solid",
+        "headerVariant": "glass",
+        "themeMode": "light",
+        "fontFamily": "inter",
+        "radius": 3,
+        "feedbackEnabled": true,
+        "footer": "Available in: 🇺🇸 🇮🇸 🇵🇹 🇳🇷 🇦🇲 & more!",
+        "allowFileUpload": true
+    },
+    "clientId": "0e1d8545-8363-4a31-943b-98333207676f",
+    "user": {
+        "data": {
+            "brand":"HASA"
+        }
+    }
+});
